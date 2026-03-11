99c3e8dc0db010befb5d8731f47daf645e762eca
Cloud Build Deploy <deploy@cloudbuild.com>
2026-03-11 23:00:38 +0300

    Fix: Redirect to root path on logout

--- js/api-client.js
+++ js/api-client.js
@@ -41,7 +41,7 @@ class AuthManager {
         localStorage.removeItem('user');
         // Clear auth cookie
         document.cookie = 'auth_token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
-        window.location.href = '/login.html';
+        window.location.href = '/';
     }
 }
 
